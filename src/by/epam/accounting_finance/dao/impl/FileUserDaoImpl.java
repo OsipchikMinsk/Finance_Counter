096927673fb70b804fe3b100819f9162cd317c1d
Artem <Osipchik@tut.by>
2019-10-27 21:31:18 +0300

    Add files via upload

---
+++ src/by/epam/accounting_finance/dao/impl/FileUserDaoImpl.java
@@ -0,0 +1,22 @@
+package by.epam.accounting_finance.dao.impl;
+import by.epam.accounting_finance.beans.User;
+import by.epam.accounting_finance.dao.UserDao;
+
+public class FileUserDaoImpl implements UserDao {
+
+
+    @Override
+    public User getUser(String name) {
+        return null;
+    }
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+}
